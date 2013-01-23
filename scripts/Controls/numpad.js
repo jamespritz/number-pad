812a17ccee3541a70759fbbaa3a7e9d5735709f8
unknown <JamieP@JAMESMACDADDY.ceinetwork.net>
2013-01-22 21:22:47 -0500

    add design, stub demo

--- scripts/Controls/numpad.js
+++ scripts/Controls/numpad.js
@@ -1,1 +1,201 @@
 ﻿
+(function ($) {
+
+    $(document).ready(function () {
+
+        $.extend(true, window, { jp: { numpad: new numpad() } });
+
+        var KEY_ESC = 27, KEY_RTRN = 13, KEY_BKSPC = 8, KEY_DEL = 46;
+
+
+        function numpad() {
+
+            $.extend(this, {
+                'show': _show
+                , 'hide': _hide
+            });
+
+
+            //log util
+            function Log(message) {
+                try {
+                    if (options.debug) {
+                        console.log(message);
+                    }
+                } catch (e) {
+                    //do nothing
+                }
+            }
+
+            var template = Handlebars.templates['buttonPad']({});
+
+            var visible = false;
+            var $_content;
+            var $_numpad;
+            var $_this;
+            var cb;
+            var options;
+
+
+            function _init() {
+
+                $_numpad = $(template);
+                $_numpad.appendTo('body');
+                $_content = $_numpad.find('.content');
+
+
+                $_numpad.on('touchstart click', '.key', handleNumPadTouchOrClick)
+                    .click(function (e) { e.stopPropagation(); });
+
+
+
+                $('html').bind('touchstart click', _hide)
+                    .keyup(function (e) {
+
+                        if (visible) {
+                            switch (event.keyCode) {
+
+                                case KEY_ESC:
+                                    Edit('OK');
+                                    cancelEvent(event);
+                                    break;
+                                case KEY_RTRN:
+                                    Edit('OK');
+                                    cancelEvent(event);
+                                    break;
+                                case KEY_BKSPC:
+                                    return false;
+                                    break;
+                                case KEY_DEL:
+                                    return false;
+                                    break;
+                                default:
+                                    if ((event.keyCode >= 48) || (event.keyCode <= 57)) {
+                                        Edit(String.fromCharCode(event.keyCode));
+                                        cancelEvent(event);
+                                        break;
+                                    }
+                            }
+                        }
+                    }).keydown(function (e) {
+
+                        if (visible) {
+                            switch (event.keyCode) {
+
+                                case KEY_ESC:
+                                    Edit('OK');
+                                    cancelEvent(event);
+                                    break;
+                                case KEY_RTRN:
+                                    Edit('OK');
+                                    cancelEvent(event);
+                                    break;
+                                case KEY_BKSPC:
+                                    Edit('Del');
+                                    return false;
+                                    break;
+                                case KEY_DEL:
+                                    Edit('Del');
+                                    return false;
+                                    break;
+
+                                default:
+
+
+                            }
+                        }
+                    });
+
+            }
+
+            function _hide(e) {
+
+                ClickOrTouch(e, function (e) {
+                    $_numpad.hide('fast'); visible = false;
+
+                });
+
+
+            }
+
+            function _show(settings) {
+
+                options = $.extend({
+                    debug: false
+                    , callBack: function (e) { alert("Callback Required"); }
+                    , includeDecimal: false
+                    , maxLength: 4
+                    , x: 0
+                    , y: 0
+                }, settings);
+
+                Log('{0} {1}'.format($(window).width(), $(window).height()));
+
+                if (options.x == 0) {
+                    options.x = ($(window).width() - $_numpad.width()) / 2;
+                }
+                if (options.y == 0) {
+                    options.y = (($(window).height() - $_numpad.height()) / 2) + $('body').scrollTop();
+                }
+
+                $_numpad.css("left", options.x).css("top", options.y).show('fast');
+                $_content.text('0');
+                cb = options.callBack;
+                visible = true;
+
+                cancelEvent(event);
+
+            }
+
+
+            function handleNumPadTouchOrClick(e) {
+
+
+                ClickOrTouch(e, function (e) {
+                    console.log($(e.srcElement).text());
+                    var key = $(e.srcElement).text();
+                    Edit(key);
+                    cancelEvent(e);
+                });
+
+
+
+            }
+
+            function Edit(key) {
+                var txt = $_content.text().trim();
+                switch (key) {
+                    case 'Del':
+
+                        if (txt.length == 1)
+                            $_content.text('0');
+                        else if ((txt != '0') && (txt.length > 0))
+                            $_content.text(txt.substr(0, txt.length - 1));
+
+                        break;
+                    case 'C':
+                        $_content.text('0');
+                        break;
+                    case 'OK':
+
+                        cb(txt);
+                        $_numpad.hide('fast');
+                        break;
+
+                    default:
+                        if ((key != '0') || (txt.length > 0)) {
+                            if (txt == '0') txt = '';
+                            if ((txt + key).length <= options.maxLength) {
+                                $_content.text(txt + key);
+                            }
+                        }
+                        break;
+                }
+            }
+
+            _init();
+        }
+    });
+
+
+})(jQuery);
---
+++ scripts/util.js
@@ -0,0 +1,134 @@
+﻿/*
+Author: Dave Gardyasz -- http://lot224.net
+Helper Function: ensures an event is properly terminated.
+*/
+function cancelEvent(e) {
+    e = !e ? window.event : e;
+
+    if (e.stopPropagation)
+        e.stopPropagation();
+    if (e.preventDefault)
+        e.preventDefault();
+    e.cancelBubble = true;
+    e.cancel = true;
+    e.returnValue = false;
+    return false;
+}
+
+
+
+function ClickOrTouch(evt, handler) {
+
+    var $el = $(evt.target);
+    var touched = false;
+    touched = $el.data('touched');
+
+    if (evt.type == 'touchstart') {
+        //set touched attr to avoid click event
+        $el.data('touched', true);
+        //relay to handler
+        handler(evt);
+    } else if (evt.type == 'click') {
+        if (touched) {
+            //don't execute event, since caller already handled touch
+            $el.data('touched', false);
+            return false;
+        } else {
+            //relay event
+            handler(evt);
+        }
+    }
+
+}
+
+function isFunction(check) {
+    var getClass = {};
+    return check && getClass.toString.call(check) == '[object Function]';
+}
+
+/*
+Author: Dave Gardyasz -- http://lot224.net
+Helper Functions: String format methods
+*/
+if (!String.prototype.format) {
+    String.prototype.format = function () {
+        var n = this;
+        for (var i = 0; i < arguments.length; i++) {
+            var e = new RegExp('\\{' + (i) + '\\}', 'gm');
+            n = n.replace(e, arguments[i]);
+        }
+        return n;
+    }
+}
+
+if (!String.format) {
+    String.format = function () {
+        for (var i = 1; i < arguments.length; i++) {
+            var e = new RegExp('\\{' + (i - 1) + '\\}', 'gm');
+            arguments[0] = arguments[0].replace(e, arguments[i]);
+        }
+        return arguments[0];
+    }
+}
+
+
+function WindowToCanvas(canvas, x, y) {
+    var bbox = canvas.getBoundingClientRect();
+    return {
+        x: (x - bbox.left) * (canvas.width / bbox.width),
+        y: (y - bbox.top) * (canvas.height / bbox.height)
+    };
+}
+
+
+function CanvasToWindow(canvas, x, y) {
+    var bbox = canvas.getBoundingClientRect();
+    return {
+        x: (x * (bbox.width / canvas.width)) + bbox.left,
+        y: (y * (bbox.height / canvas.height)) + bbox.top
+    };
+}
+
+function PagePos(e) {
+
+    var posx = 0;
+    var posy = 0;
+    if (!e) var e = window.event;
+    if (e.pageX || e.pageY) {
+        posx = e.pageX;
+        posy = e.pageY;
+    }
+    else if (e.clientX || e.clientY) {
+        posx = e.clientX + document.body.scrollLeft + document.documentElement.scrollLeft;
+        posy = e.clientY + document.body.scrollTop + document.documentElement.scrollTop;
+    }
+
+    return { x: posx, y: posy };
+
+}
+
+/*
+Author: James Pritz -- http://www.jamespritz.com
+Helper Function: conversion between em and px for exact measurement
+*/
+$.fn.toEm = function (settings) {
+    settings = jQuery.extend({
+        scope: 'body'
+    }, settings);
+    var that = parseInt(this[0], 10),
+        scopeTest = jQuery('<div style="display: none; font-size: 1em; margin: 0; padding:0; height: auto; line-height: 1; border:0;">&nbsp;</div>').appendTo(settings.scope),
+        scopeVal = scopeTest.height();
+    scopeTest.remove();
+    return (that / scopeVal).toFixed(8);
+};
+
+$.fn.toPx = function (settings) {
+    settings = jQuery.extend({
+        scope: 'body'
+    }, settings);
+    var that = parseFloat(this[0]),
+        scopeTest = jQuery('<div style="display: none; font-size: 1em; margin: 0; padding:0; height: auto; line-height: 1; border:0;">&nbsp;</div>').appendTo(settings.scope),
+        scopeVal = scopeTest.height();
+    scopeTest.remove();
+    return Math.round(that * scopeVal);
+};
